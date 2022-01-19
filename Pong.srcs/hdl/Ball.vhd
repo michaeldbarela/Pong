3f1bb27a410b2c3d77731c0bca3bc0bc21d53449
Michael Barela <michael.d.barela@gmail.com>
2022-01-18 16:16:56 -0800

    Added logic to mve the ball

--- Pong.srcs/hdl/Ball.vhd
+++ Pong.srcs/hdl/Ball.vhd
@@ -37,7 +37,7 @@ entity Ball is
         reset       : in std_logic;
         tick        : in std_logic;
         delay       : in std_logic;
-        select      : in std_logic_vector(3 downto 0);
+        sel         : in std_logic_vector(3 downto 0);
         SW_red      : in std_logic_vector(3 downto 0);
         SW_gre      : in std_logic_vector(3 downto 0);
         SW_blu      : in std_logic_vector(3 downto 0);
@@ -57,6 +57,35 @@ entity Ball is
 end Ball;
 
 architecture Behavioral of Ball is 
+----------------------------------------------------------------------------------
+-- CONSTANT DECLARATIONS
+----------------------------------------------------------------------------------
+    constant BALL_SIZE      : positive := 3;
+    -- length of the left wall left side
+    constant LWL            : positive := 15;
+    -- length of the left wall right side
+    constant LWR            : positive := 20;
+    -- length of the left wall top side 1
+    constant LWT1           : positive := 40;
+    -- length of the left wall bottom side 1
+    constant LWB1           : positive := 210;
+    -- length of the left wall top side 2
+    constant LWT2           : positive := 270;
+    -- length of the left wall bottom side 2
+    constant LWB2           : positive := 461;
+    -- length of the right paddle left side
+    constant RPL            : positive := 603;
+    -- length of the right paddle right side
+    constant RPR            : positive := 608;
+    -- length of the left paddle left side
+    constant LPL            : positive := 15;
+    -- length of the left paddle right side
+    constant LPR            : positive := 20;
+    -- length of the top wall bottom side
+    constant TWB            : positive := 39;
+    -- length of the bottom wall top side
+    constant BWT            : positive := 462;
+
 ----------------------------------------------------------------------------------
 -- SIGNAL DECLARATIONS
 ----------------------------------------------------------------------------------
@@ -86,16 +115,74 @@ architecture Behavioral of Ball is
     signal ball_x_D         : std_logic_vector(9 downto 0);
     signal ball_y_Q         : std_logic_vector(9 downto 0);
     signal ball_y_D         : std_logic_vector(9 downto 0);
-    variable ball_size      : positive := 3;
-
 
 begin
 ----------------------------------------------------------------------------------
 -- SEQUENTIAL LOGIC
 ----------------------------------------------------------------------------------
+    -- current-state logic
+    process(clk, reset) begin
+        if(rising_edge(reset)) then
+            pause_Q <= '1'; -- start game in a paused state
+            ball_x_Q <= std_logic_vector(to_unsigned(320, ball_x_Q'length)); -- start at this x-coordinate
+            ball_y_Q <= std_logic_vector(to_unsigned(240, ball_y_Q'length)); -- start at this y-coordinate
+            x_dir_Q <= '0'; -- start going left
+            y_dir_Q <= '0'; -- start going down
+            score_l_Q <= (OTHERS=>'0'); -- start left score at 0
+            score_r_Q <= (OTHERS=>'0'); -- start right score at 0
+        elsif(rising_edge(clk)) then
+            pause_Q <= pause_D;
+            ball_x_Q <= ball_x_D;
+            ball_y_Q <= ball_y_D;
+            x_dir_Q <= x_dir_D;
+            y_dir_Q <= y_dir_D;
+            score_l_Q <= score_l_D;
+            score_r_Q <= score_r_D;
+        end if;
+    end process;
 
 ----------------------------------------------------------------------------------
 -- COMBINATIONAL LOGIC
 ----------------------------------------------------------------------------------
+    process 
+        variable concat : std_logic_vector(11 downto 0) := std_logic_vector(SW_red & SW_gre & SW_blu);
+    begin
+        -- determine color of asset based on switches
+        case(sel(2 downto 0))is 
+            when "000" => ball_rgb <= (OTHERS=>'0');
+            when "001" => ball_rgb <= concat;
+            when others => ball_rgb <= ball_rgb;
+        end case;
+
+        -- detenmine next location of the ball
+        if(pause_Q = '1') then
+            pause_D <= '0' when (delay='1') else '0'; -- pause for three sec if 1
+            ball_x_D <= ball_x_Q; -- ball will not move
+            ball_y_D <= ball_y_Q; -- ball will not move
+        elsif(tick = '1') then
+            pause_D <= '0'; -- game is no longer paused
+            ball_x_D <= std_logic_vector(unsigned(ball_x_Q) + 1) when (x_dir_Q='1') else std_logic_vector(unsigned(ball_x_Q) + 1);
+            ball_y_D <= std_logic_vector(unsigned(ball_y_Q) + 1) when (y_dir_Q='1') else std_logic_vector(unsigned(ball_y_Q) + 1);
+        else
+            pause_D <= '0'; -- game is no longer paused
+            ball_x_D <= ball_x_Q; -- ball will move
+            ball_y_D <= ball_y_Q; -- ball will move
+        end if;
+
+        -- resets ball to center after a goal, pauses, and increments score
+
+        -- changes direction after a bounce
+        --  x: =0 means left; =1 means right
+        --  y: =0 means down; =1 means up
+
+    end process;
+
+    -- detemine ball boundaries
+
+    -- determine if ball hits an object
+
+    -- determine if there was a goal
+
+    -- assert if ball is within range of pixel x and pixel y
 
 end Behavioral;
